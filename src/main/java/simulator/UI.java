534c749eeb469cdd70b1c639667214ee23fda073
Marshall Thompson <mdt_@gwu.edu>
2022-09-20 15:16:44 -0400

    refactor UI for project structure

--- src/main/java/simulator/UI.java
+++ src/main/java/simulator/UI.java
@@ -1,3 +1,5 @@
+package simulator;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
---
+++ src/test/java/simulator/UITest.java
@@ -0,0 +1,10 @@
+package simulator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UITest {
+    @org.junit.jupiter.api.Test
+    void placeHolderTest(){
+        assertEquals(true, true);
+    }
+}
