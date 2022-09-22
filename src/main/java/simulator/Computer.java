1e5bbd646cddc7ef3c22b3d096b05d54ca30a9ef
Marshall Thompson <mdt_@gwu.edu>
2022-09-22 11:07:17 -0400

    add memory and register initilization to computer

--- src/main/java/simulator/Computer.java
+++ src/main/java/simulator/Computer.java
@@ -1,13 +1,31 @@
 package simulator;
 
+import java.util.ResourceBundle;
+
 public class Computer {
     int wordSize;
-//    Memory dram;
+    DRAM dram;
+
+    // Registers
+    Register[] GPRs;
+    Register[] IXs;
+
+    // UI
     UI ui;
 
-    public Computer(int wordSize, int dramSize) {
+    public Computer(int wordSize, int dramSize, int numGPR, int numIX) {
         this.wordSize = wordSize;
-//        this.dram = new Memory(wordSize, dramSize);
+        this.dram = new DRAM(wordSize, dramSize);
+
+        // Initialize GPRs
+        this.GPRs = new Register[numGPR];
+        for (int i = 0; i < numGPR; i++)
+            this.GPRs[i] = new Register(this.wordSize);
+
+        // Initialize IX Registers
+        this.IXs = new Register[numIX];
+        for (int i = 0; i < numIX; i++)
+            this.IXs[i] = new Register(this.wordSize);
 
         // Start UI
         this.ui = new UI();
@@ -15,7 +33,7 @@ public class Computer {
     }
 
     public static void main(String[] args) {
-        Computer cpu = new Computer(16, 2048);
+        Computer cpu = new Computer(16, 2048, 4, 3);
     }
 }
 
