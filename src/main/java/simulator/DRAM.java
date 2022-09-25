cd9cc17b7c2e5ea4b5c7e31b3fa6151918c9360e
Marshall Thompson <mdt_@gwu.edu>
2022-09-24 20:05:49 -0400

    comments

--- src/main/java/simulator/DRAM.java
+++ src/main/java/simulator/DRAM.java
@@ -3,6 +3,9 @@ package simulator;
 import java.util.Arrays;
 
 public class DRAM {
+    /*
+    DRAM class to manage main memory. Handles loads, stores, and the calculation of the effective address
+     */
     int numberOfWords;
     int wordSize;
 
@@ -18,18 +21,33 @@ public class DRAM {
     }
 
     public int load(Register MAR, Register MBR, Register IX, int I, String type) {
+        /*
+        MAR - memory address register
+        MBR - memory buffer register
+
+        IX - index register
+        I - int, indirect bit
+
+        Type - R, A or X for LDA, LDR, LDX
+
+        This function handles the loading of a value from memory at an address stored in the MAR,
+        to the MBR. It handles all three types of Loads.
+         */
         int address;
         int EA;
         int returnCode;
 
+        // get address from MAR and calculate the effective address
         address = Helper.arrToInt(MAR.getRegisterValue());
         EA = this.calculateEffectiveAddress(address, IX, I);
 
+        // validate address if fault occurs return with fault code
         returnCode = this.checkAddress(EA);
         if (returnCode != 0) {
             return returnCode;
         }
 
+        // load the proper value from memory. R for LDR. X for LDX, A for LDA
         if (type.toUpperCase().compareTo("R") == 0 || type.toUpperCase().compareTo("X") == 0) {
             if (type.toUpperCase().compareTo("X") == 0) {
                 EA = Helper.arrToInt(MAR.getRegisterValue());
@@ -40,22 +58,37 @@ public class DRAM {
             int [] binaryEA = Helper.intToBinArray(EA, this.wordSize);
             MBR.setRegisterValue(binaryEA);
         } else {
+            // machine fault 4
             returnCode = 4;
         }
-
+        // default no machine fault
         return returnCode;
     }
 
     public int store(Register MAR, Register MBR, Register IX, int I, String type) {
+        /*
+        MAR - memory address register
+        MBR - memory buffer register
+
+        IX - index register
+        I - int, indirect bit
+
+        Type - R, A or X for LDA, LDR, LDX
+
+        This function handles the loading of a value from memory at an address stored in the MAR,
+        to the MBR. It handles all two types of stores.
+         */
         int address;
         int EA;
         int returnCode;
 
+        // get address from MAR and calculate the effective address
         address = Helper.arrToInt(MAR.getRegisterValue());
         EA = this.calculateEffectiveAddress(address, IX, I);
 
         System.out.println(String.format("Computed EA: %d", EA));
 
+        // validate address if fault occurs return with fault code
         returnCode = this.checkAddress(EA);
         if (returnCode != 0) {
             return returnCode;
@@ -65,6 +98,7 @@ public class DRAM {
             if (type.toUpperCase().compareTo("X") == 0) {
                 EA = Helper.arrToInt(MAR.getRegisterValue());
             }
+            // copy data
             int[] regData = MBR.getRegisterValue();
             for (int i = 0; i < this.wordSize; i++) {
                 this.data[i+EA] = regData[i];
@@ -78,6 +112,10 @@ public class DRAM {
     }
 
     private int calculateEffectiveAddress(int address, Register IX, int I) {
+        /*
+        This is adapted from the psuedocode provided in the project outline
+        given by Professor Lancaster.
+         */
         if (I == 0) {
             if (IX == null) {
                 return address * this.wordSize;
@@ -100,26 +138,42 @@ public class DRAM {
     }
 
     public void memset(int[] value, int start) {
+        /*
+        Set a word sized block of memory to the value in the value array
+         */
         for (int i = start; i < start + value.length; i++) {
             this.data[i] = value[i-start];
         }
     }
 
     public void memNuke() {
+        /*
+        Set all memory after reserved words to 0.
+         */
         for (int i = 6; i < this.size; i++) {
             this.data[i] = 0;
         }
     }
 
     int fetchAddress(int EA) {
+        /*
+        Get the integer value for the word at the effective address EA
+         */
         return Helper.arrToInt(Arrays.copyOfRange(this.data, EA, EA+wordSize));
     }
 
     int[] fetchBinaryValue(int EA) {
+        /*
+        Get the binary array for the word at the effective address EA
+         */
         return Arrays.copyOfRange(this.data, EA, EA+wordSize);
     }
 
     private int checkAddress(int address)  {
+        /*
+        Validate address. Make sure its not on reserved word, and make sure it doesnt
+        exceed bounds. return the proper fault if it does.
+         */
         int endAddress = address + this.wordSize;
         if (address >= 0 && address <= 5) {
             return 1;
--- src/main/java/simulator/Instructions.java
+++ src/main/java/simulator/Instructions.java
@@ -6,6 +6,18 @@ import java.util.Arrays;
 public class Instructions {
     public static int LDR(DRAM dram, Register MAR, Register MBR, Register IX, Register registerTarget,
                           int indirect, Register MFR) {
+        /*
+        dram - pointer to dram object
+        MAR - mar register that holds address
+        MBR - MBR register
+        IX- IX register to be used
+        registerTarget - register to be loaded/stored to/from
+        indirect - indirect bit
+        MFR - machine fault register
+
+        Function to execute the LDR instruction. Calls dram load and sets machine fault register MFR.
+        Sets the target register value.
+         */
         System.out.println("------------------------------");
         System.out.println("LDR CALLED");
         System.out.println(String.format("MBR VALUE %s", Arrays.toString(MBR.getRegisterValue())));
@@ -36,6 +48,18 @@ public class Instructions {
 
     public static int LDA(DRAM dram, Register MAR, Register MBR, Register IX, Register registerTarget,
                           int indirect, Register MFR) {
+        /*
+        dram - pointer to dram object
+        MAR - mar register that holds address
+        MBR - MBR register
+        IX- IX register to be used
+        registerTarget - register to be loaded/stored to/from
+        indirect - indirect bit
+        MFR - machine fault register
+
+        Function to execute the LDA instruction. Calls dram load and sets machine fault register MFR.
+        Sets the target register value.
+         */
         System.out.println("------------------------------");
         System.out.println("LDA CALLED");
         System.out.println(String.format("MBR VALUE %s", Arrays.toString(MBR.getRegisterValue())));
@@ -64,6 +88,18 @@ public class Instructions {
     }
 
     public static int LDX(DRAM dram, Register MAR, Register MBR, Register IX, int indirect, Register MFR) {
+        /*
+        dram - pointer to dram object
+        MAR - mar register that holds address
+        MBR - MBR register
+        IX- IX register to be used
+        registerTarget - register to be loaded/stored to/from
+        indirect - indirect bit
+        MFR - machine fault register
+
+        Function to execute the LDX instruction. Calls dram load and sets machine fault register MFR.
+        Sets the target register value.
+         */
         System.out.println("------------------------------");
         System.out.println("LDX CALLED");
         System.out.println(String.format("MBR VALUE %s", Arrays.toString(MBR.getRegisterValue())));
@@ -92,6 +128,18 @@ public class Instructions {
 
     public static int STR(DRAM dram, Register MAR, Register MBR, Register IX, Register registerTarget,
                           int indirect, Register MFR) {
+        /*
+        dram - pointer to dram object
+        MAR - mar register that holds address
+        MBR - MBR register
+        IX- IX register to be used
+        registerTarget - register to be loaded/stored to/from
+        indirect - indirect bit
+        MFR - machine fault register
+
+        Function to execute the STR instruction. Calls dram load and sets machine fault register MFR.
+        Sets the target register value.
+         */
         System.out.println("------------------------------");
         MBR.setRegisterValue(registerTarget.getRegisterValue());
         System.out.println("STR INSTRUCTION CALLED");
@@ -122,6 +170,18 @@ public class Instructions {
 
     public static int STX(DRAM dram, Register MAR, Register MBR, Register IX,
                           int indirect, Register MFR) {
+        /*
+        dram - pointer to dram object
+        MAR - mar register that holds address
+        MBR - MBR register
+        IX- IX register to be used
+        registerTarget - register to be loaded/stored to/from
+        indirect - indirect bit
+        MFR - machine fault register
+
+        Function to execute the STX instruction. Calls dram load and sets machine fault register MFR.
+        Sets the target register value.
+         */
         // store the IX value in the MBR
         System.out.println("------------------------------");
 
